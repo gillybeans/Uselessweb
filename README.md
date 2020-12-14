d87c1c4275b489a0fc58755f662815b1d952054e
Gilda A <gilda.ahmadniaye@gmail.com>
2020-12-14 21:43:40 +0100

    idk, last stuff maybe i guess

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # The Useless Web
-Assignment in JavaScript!
+<img src="https://media.giphy.com/media/12gaOPj4dsnBTO/giphy.gif" width="100%">
+
+## About
+This is an assignment to create a useless web, a website which serves no purpose but still makes the user intrigued. No third party libraries such as jQuery or React was allowed.
 
 ## The code includes:
 <ul>
@@ -9,7 +12,9 @@ Assignment in JavaScript!
   </ul>
 
 ## Live / Installation
-Blah blah
+Clone the repository to your computer
+<pre></pre><code>$ git clone https://github.com/gillybeans/Uselessweb.git </code></pre>
+Or just go directly to the live version <a href="https://splish-splash.netlify.app/">HERE</a>
 
 ## Created by
 * Gilda Ahmadniaye 
--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var theFish = document.querySelector("#fish");
-var container = document.querySelector("#contentContainer");
+let theFish = document.querySelector("#fish");
+let container = document.querySelector("#contentContainer");
  
 container.addEventListener("click", getClickPosition, false);
  
 function getClickPosition(e) {
-    var parentPosition = getPosition(e.currentTarget);
-    var xPosition = e.clientX - parentPosition.x - (theFish.clientWidth / 2);
-    var yPosition = e.clientY - parentPosition.y - (theFish.clientHeight / 2);
+    let parentPosition = getPosition(e.currentTarget);
+    let xPosition = e.clientX - parentPosition.x - (theFish.clientWidth / 2);
+    let yPosition = e.clientY - parentPosition.y - (theFish.clientHeight / 2);
      
     theFish.style.left = xPosition + "px";
     theFish.style.top = yPosition + "px";
@@ -14,14 +14,14 @@ function getClickPosition(e) {
  
 // function to get the element's exact position
 function getPosition(el) {
-  var xPos = 0;
-  var yPos = 0;
+  let xPos = 0;
+  let yPos = 0;
  
   while (el) {
     if (el.tagName == "BODY") {
       // browser quirks with body/window/document and page scroll
-      var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
-      var yScroll = el.scrollTop || document.documentElement.scrollTop;
+      let xScroll = el.scrollLeft || document.documentElement.scrollLeft;
+      let yScroll = el.scrollTop || document.documentElement.scrollTop;
  
       xPos += (el.offsetLeft - xScroll + el.clientLeft);
       yPos += (el.offsetTop - yScroll + el.clientTop);
@@ -38,6 +38,8 @@ function getPosition(el) {
     y: yPos
   };
 }
+
+
 
 /****** ZOOM ZOOM ********/
 function zoom(event) {
@@ -59,21 +61,3 @@ function zoom(event) {
 
 
 
-
-  /*var direction = "",
-    oldx = 0,
-    mousemovemethod = function (e) {
-    
-        if (e.pageX < oldx) {
-            direction = "left"
-        } else if (e.pageX > oldx) {
-            direction = "right"
-        }
-        
-        document.body.innerHTML = direction;
-        
-        oldx = e.pageX;
-        
-}
-
-document.addEventListener('mousemove', mousemovemethod);*/
